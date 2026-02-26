cc4c7f4eebf048678a017c97ef829a360962a317
nuke1115 <nuclearbomb280@gmail.com>
2026-02-26 17:44:31 +0900

    add:SoundChannel enum추가

---
+++ Fistival!!!/Assets/@Scripts/Defines/SoundChannel.cs
@@ -0,0 +1,16 @@
+namespace Defines
+{
+    public enum SoundChannel
+    {
+        BGM_0,
+        BGM_1,
+        SUB_BGM_0,
+        SUB_BGM_1,
+        SUB_BGM_2,
+        EFFECT_0,
+        EFFECT_1,
+        EFFECT_2,
+        UNKNOWN=-1,
+        CHANNEL_CNT = 8
+    }
+}
