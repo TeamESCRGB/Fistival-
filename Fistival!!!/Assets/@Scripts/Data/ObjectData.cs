374808a99d35e0dc78988dc1737c6d3ff366f872
nuke1115 <nuclearbomb280@gmail.com>
2026-03-11 01:59:11 +0900

    feature:내구도 추가

--- Fistival!!!/Assets/@Scripts/Data/ObjectData.cs
+++ Fistival!!!/Assets/@Scripts/Data/ObjectData.cs
@@ -6,6 +6,7 @@ namespace Data
         {
             ObjectIndex = original.ObjectIndex;
             Damage = original.Damage;
+            Durability = original.Durability;
             Weight = original.Weight;
             SpriteName = original.SpriteName;
             AnimControllerName = original.AnimControllerName;
@@ -15,6 +16,7 @@ namespace Data
 
         public int ObjectIndex;
         public int Damage;
+        public int Durability;
         public float Weight;//날아가는 속도에 영향.
         public string SpriteName;
         public string AnimControllerName;
