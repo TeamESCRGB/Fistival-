713f4dc99b92f7374a371ffaba98759bf67b3759
nuke1115 <nuclearbomb280@gmail.com>
2026-03-12 00:28:49 +0900

    feature:ModeBase추가

---
+++ Fistival!!!/Assets/@Scripts/Coordinator/ModeBase.cs
@@ -0,0 +1,29 @@
+using Data;
+using UnityEngine;
+
+namespace Coordinator
+{
+    public abstract class ModeBase : MonoBehaviour
+    {
+        protected PlayerInputCoordinator _inputCoordinator;
+        protected CommonModeData _commonData;
+
+        public virtual void Init(CommonModeData data)
+        {
+            gameObject.SetActive(true);
+            _inputCoordinator.Init();
+            _commonData = data;
+
+        }
+
+        public virtual void DeInit()
+        {
+            gameObject.SetActive(false);
+        }
+
+        public CommonModeData GetSharedData()
+        {
+            return _commonData;
+        }
+    }
+}
