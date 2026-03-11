20df5ea81b1e12fc139791e1f2fa89153d5b1183
nuke1115 <nuclearbomb280@gmail.com>
2026-03-11 14:12:31 +0900

    feature: Object타입 투척물 기본 코디네이터 추가

---
+++ Fistival!!!/Assets/@Scripts/Coordinator/ObjectCoordinator.cs
@@ -0,0 +1,117 @@
+using Data;
+using Manager;
+using UnityEngine;
+using Utils;
+
+namespace Coordinator
+{
+    public class ObjectCoordinator : MonoBehaviour
+    {
+        //추가 예정인 것: 소리(날아가는거, 충돌, 파괴), 파티클(날아가는거, 충돌, 파괴), 애니메이션
+        private ObjectData _data;
+        private Rigidbody2D _rb2d;
+        private Collider2D _col2d;
+        private int _durability = 1;
+        private int _abrasableLayerMask = 1;
+
+        private void Awake()
+        {
+            _rb2d = gameObject.GetOrAddComponent<Rigidbody2D>();
+            _col2d = gameObject.GetOrAddComponent<Collider2D>();
+        }
+
+        public virtual void Init(ObjectData data)
+        {
+            if(data == null)
+            {
+#if UNITY_EDITOR
+                Debug.LogError("data null");
+#endif
+                return;
+            }
+            _data = data;
+            transform.SetParent(null, false);
+            _rb2d.bodyType = RigidbodyType2D.Dynamic;
+            _col2d.enabled = true;
+            _rb2d.mass = data.Weight;
+            _rb2d.linearVelocity = Vector2.zero;
+            _rb2d.angularVelocity = 0;
+            _durability = data.Durability;
+            _abrasableLayerMask = data.AbrasableLayerMask;
+        }
+
+        public virtual bool Throw(in Vector2 dir,in Vector2 parentLinVelocity ,float force)
+        {
+            if(Drop(parentLinVelocity) == false)
+            {
+                return false;
+            }
+            _rb2d.AddForce(dir*force,ForceMode2D.Impulse);
+            return true;
+        }
+
+        public virtual bool Smash()
+        {
+            _durability--;
+            if(_durability <= 0)
+            {
+                Managers.Instance.ResourceManager.Destroy(gameObject);
+                return false;
+            }
+            return true;
+        }
+
+        public virtual bool Drop(in Vector2 parentLinVelocity)
+        {
+            _rb2d.bodyType = RigidbodyType2D.Dynamic;
+            _col2d.enabled = true;
+            transform.SetParent(null, true);
+            _rb2d.linearVelocity = parentLinVelocity;
+            return true;
+        }
+
+        public virtual bool PickUp(Transform anchor)
+        {
+            if(anchor == null)
+            {
+#if UNITY_EDITOR
+                Debug.LogError("anchor is null");
+#endif
+                return false;
+            }
+            else
+            {
+                _rb2d.linearVelocity = Vector2.zero;
+                _rb2d.angularVelocity = 0;
+                transform.SetParent(anchor,false);
+                transform.SetLocalPositionAndRotation(Vector3.zero, Quaternion.identity);
+                _rb2d.bodyType = RigidbodyType2D.Kinematic;
+                _col2d.enabled = false;
+                return true;
+            }
+        }
+
+        protected virtual void InternalCollisionHandler(Collision2D col)
+        {
+            if (col == null || col.gameObject == null)
+            {
+                return;
+            }
+            
+            if(((1<<col.gameObject.layer) & _abrasableLayerMask) != 0)
+            {
+                _durability--;
+            }
+
+            if(_durability <= 0)
+            {
+                Managers.Instance.ResourceManager.Destroy(gameObject);
+            }
+        }
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            InternalCollisionHandler(collision);
+        }
+    }
+}
