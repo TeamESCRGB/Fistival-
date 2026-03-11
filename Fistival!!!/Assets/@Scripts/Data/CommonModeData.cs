b96eb151fc7a7e8c8e8d5e8203e945e96e6367b4
nuke1115 <nuclearbomb280@gmail.com>
2026-03-11 22:53:11 +0900

    feature:CommonModeData추가

---
+++ Fistival!!!/Assets/@Scripts/Data/CommonModeData.cs
@@ -0,0 +1,21 @@
+namespace Data
+{
+    public class CommonModeData
+    {
+        public CommonModeData(CommonModeData original)
+        {
+            CommonModeDataIdx = original.CommonModeDataIdx;
+            Damage = original.Damage;
+            //ThrowPower = original.ThrowPower;
+            AttackCooldown = original.AttackCooldown;
+            AnimControllerName = original.AnimControllerName;
+        }
+        public CommonModeData() { }
+
+        public int CommonModeDataIdx;
+        public int Damage;
+        //public float ThrowPower;
+        public float AttackCooldown;
+        public string AnimControllerName;
+    }
+}
