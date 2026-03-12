510c29a54ed4613e92e6405d6afe08fa3a1d1ca5
nuke1115 <nuclearbomb280@gmail.com>
2026-03-12 16:57:27 +0900

    feature:PlatformerMovementCoordinator추가

--- Fistival!!!/Assets/@Scripts/Coordinator/Modes/FistivalMode.cs
+++ Fistival!!!/Assets/@Scripts/Coordinator/Modes/FistivalMode.cs
@@ -8,8 +8,10 @@ namespace Coordinator.Modes
     public class FistivalMode : ModeBase, IRMBInputHandler, ILMBInputHandler, IDropInputHandler
     {
         private HandCoordinator _hand;
+        private PlatformerMovementCoordinator _movCoordinator;
         private void Awake()
         {
+            _movCoordinator = gameObject.GetOrAddComponent<PlatformerMovementCoordinator>();
             ModeType = Defines.ModeTypes.FISTIVAL;
             _hand = GetComponentInChildren<HandCoordinator>();
             _inputCoordinator = gameObject.GetComponentInParent<PlayerInputCoordinator>();
@@ -18,9 +20,11 @@ namespace Coordinator.Modes
         public override void Init(CommonModeData data)
         {
             base.Init(data);
+            _movCoordinator.Init(data.MoveSpeed,data.JumpPower,GetComponentInParent<Rigidbody2D>());
             _inputCoordinator.SetDropInputHandler(this);
             _inputCoordinator.SetLMBInputHandler(this);
             _inputCoordinator.SetRMBInputHandler(this);
+            _inputCoordinator.SetMovementInputHandler(_movCoordinator);
             _hand.Init(GetComponentInParent<Rigidbody2D>(), data.Damage, 0,Vector2.zero);//아니 이거 데이터에 추가해야되네
         }
 
