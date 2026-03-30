2f40a32a3242424baa4a957ea24b817a8c5298ae
nuke1115 <nuclearbomb280@gmail.com>
2026-03-31 00:53:50 +0900

    feature: 노트 및 판정에 관한 enum추가

---
+++ Fistival!!!/Assets/@Scripts/Defines/JudgementTypes.cs
@@ -0,0 +1,9 @@
+﻿namespace Defines
+{
+    public enum JudgementTypes : byte
+    {
+        PERFECT = 1,
+        GOOD = 2,
+        MISS=3
+    }
+}
---
+++ Fistival!!!/Assets/@Scripts/Defines/NoteTypes.cs
@@ -0,0 +1,9 @@
+﻿namespace Defines
+{
+    public enum NoteTypes : byte
+    {
+        SHORT_PARRY=1,
+        LONG_PARRY_START=2,
+        LONG_PARRY_END=3
+    }
+}
