cff04f7572d0567e6198a55bc76c11e96fb74a24
nuke1115 <nuclearbomb280@gmail.com>
2026-04-09 02:26:14 +0900

    feature:

--- Fistival!!!/Assets/@Scripts/Defines/NoteTypes.cs
+++ Fistival!!!/Assets/@Scripts/Defines/NoteTypes.cs
@@ -2,8 +2,12 @@
 {
     public enum NoteTypes : byte
     {
-        SHORT_PARRY=1,
-        LONG_PARRY_START=2,
-        LONG_PARRY_END=3
+        SHORT_PARRY_RDY=1,
+        SHORT_PARRY=2,
+        LONG_PARRY_RDY=3,
+        LONG_PARRY_START=4,
+        LONG_PARRY_MIDDLE=5,
+        LONG_PARRY_END=6,
+        NO_ACTION=7
     }
 }
