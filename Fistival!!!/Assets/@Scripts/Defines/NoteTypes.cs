01faac5581f8fd790d511fc84c23c54006ee4bc8
nuke1115 <nuclearbomb280@gmail.com>
2026-04-15 01:37:24 +0900

    fix: NoteTypes를 Flag

--- Fistival!!!/Assets/@Scripts/Defines/NoteTypes.cs
+++ Fistival!!!/Assets/@Scripts/Defines/NoteTypes.cs
@@ -1,13 +1,16 @@
-﻿namespace Defines
+﻿using System;
+
+namespace Defines
 {
+    [Flags]
     public enum NoteTypes : byte
     {
-        SHORT_PARRY_RDY=1,
-        SHORT_PARRY=2,
-        LONG_PARRY_RDY=3,
-        LONG_PARRY_START=4,
-        LONG_PARRY_MIDDLE=5,
-        LONG_PARRY_END=6,
-        NO_ACTION=7
+        SHORT_PARRY_RDY=1<<0,
+        SHORT_PARRY=1<<1,
+        LONG_PARRY_RDY=1<<2,
+        LONG_PARRY_START=1<<3,
+        LONG_PARRY_MIDDLE=1<<4,
+        LONG_PARRY_END=1<<5,
+        NO_ACTION=1<<6
     }
 }
