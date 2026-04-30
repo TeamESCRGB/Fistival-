cbd95f35e800316b759ca4cbfbccdb805254a513
nuke1115 <nuclearbomb280@gmail.com>
2026-04-30 17:24:16 +0900

    add:Projectile

--- Fistival!!!/Assets/@Scripts/Data/ProjectileData.cs
+++ Fistival!!!/Assets/@Scripts/Data/ProjectileData.cs
@@ -1,17 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Data
+﻿namespace Data
 {
     public class ProjectileData
     {
         public ProjectileData(ProjectileData original)
         {
             IDX = original.IDX;
+            BouncableLayerMask= original.BouncableLayerMask;
+            ExplodableLayerMask= original.ExplodableLayerMask;
+            Speed = original.Speed;
+            Damage = original.Damage;
+            Physics2DMaterialName = original.Physics2DMaterialName;
+            ProjectilePrefabName = original.ProjectilePrefabName;
         }
         public ProjectileData() { }
 
         public int IDX;
+        public int BouncableLayerMask;
+        public int ExplodableLayerMask;
+        public float Speed;
+        public int Damage;
+        public string Physics2DMaterialName;
+        public string ProjectilePrefabName;
     }
 }
