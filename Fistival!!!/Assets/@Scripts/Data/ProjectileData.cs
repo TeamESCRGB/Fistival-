3ab66bae86a7a9b71348fc20aa3c992b0f8871f7
nuke1115 <nuclearbomb280@gmail.com>
2026-04-30 15:45:40 +0900

    add:ProjectileData클래스 추가

---
+++ Fistival!!!/Assets/@Scripts/Data/ProjectileData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data
+{
+    public class ProjectileData
+    {
+        public ProjectileData(ProjectileData original)
+        {
+            IDX = original.IDX;
+        }
+        public ProjectileData() { }
+
+        public int IDX;
+    }
+}
