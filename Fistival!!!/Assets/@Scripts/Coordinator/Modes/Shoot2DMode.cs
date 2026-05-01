ebedebb9fc88dc8b007a062350b7ec6fbbbc93c6
nuke1115 <nuclearbomb280@gmail.com>
2026-05-01 14:16:29 +0900

    feature: FlightMovementCoordinator추가

--- Fistival!!!/Assets/@Scripts/Coordinator/Modes/Shoot2DMode.cs
+++ Fistival!!!/Assets/@Scripts/Coordinator/Modes/Shoot2DMode.cs
@@ -1,24 +1,40 @@
 ﻿
 
+using Assets._Scripts.Coordinator.Movements;
 using Data;
 using Defines;
 using UnityEngine;
+using Utils;
 
 namespace Coordinator.Modes
 {
     public class Shoot2DMode : ModeBase
     {
+        private FlightMovementCoordinator _movementCoord;
         public override ModeTypes ModeType => ModeTypes.SHOOT_2D;
+        private Rigidbody2D _parentrb2d;
+        private float _gravityScale;
+
+        protected override void OnAwake()
+        {
+            base.OnAwake();
+            _parentrb2d = GetComponentInParent<Rigidbody2D>();
+            _movementCoord = gameObject.GetOrAddComponent<FlightMovementCoordinator>();
+        }
 
         public override void Init(CommonModeData data)
         {
             base.Init(data);
-
+            _movementCoord.Init(data.MoveSpeed, _parentrb2d);
+            _gravityScale = _parentrb2d.gravityScale;
+            _parentrb2d.gravityScale = 0;
+            _inputCoordinator.SetHorizontalMovementInputHandler(_movementCoord);
+            _inputCoordinator.SetVerticalMovementInputHandler(_movementCoord);
         }
 
         public override void DeInit()
         {
-
+            _parentrb2d.gravityScale = _gravityScale;
             base.DeInit();
         }
 
