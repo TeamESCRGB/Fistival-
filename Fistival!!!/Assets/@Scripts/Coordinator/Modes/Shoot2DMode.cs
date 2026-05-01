6f28023e04e614a962538446f8b2606ec16e0b4b
nuke1115 <nuclearbomb280@gmail.com>
2026-05-01 11:47:20 +0900

    add: 2D

---
+++ Fistival!!!/Assets/@Scripts/Coordinator/Modes/Shoot2DMode.cs
@@ -0,0 +1,40 @@
+﻿
+
+using Data;
+using Defines;
+using UnityEngine;
+
+namespace Coordinator.Modes
+{
+    public class Shoot2DMode : ModeBase
+    {
+        public override ModeTypes ModeType => ModeTypes.SHOOT_2D;
+
+        public override void Init(CommonModeData data)
+        {
+            base.Init(data);
+
+        }
+
+        public override void DeInit()
+        {
+
+            base.DeInit();
+        }
+
+        public override void OnDropEvent(bool pressed)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void OnLMBEvent(bool pressed, Vector2 screenPos)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void OnRMBEvent(bool pressed, Vector2 screenPos)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
--- Fistival!!!/Assets/@Scripts/Defines/ModeTypes.cs
+++ Fistival!!!/Assets/@Scripts/Defines/ModeTypes.cs
@@ -5,6 +5,7 @@
         UNKNOWN=-6,
         FISTIVAL=1,
         PLATFORMER=2,
-        RHYTHM=3
+        RHYTHM=3,
+        SHOOT_2D=4
     }
 }
