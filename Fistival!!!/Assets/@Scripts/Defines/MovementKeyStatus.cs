3a2c95177df1ca5ea569ed0c88638e8dc002d0cc
nuke1115 <nuclearbomb280@gmail.com>
2026-05-05 16:24:02 +0900

    add: MovementKeyStatus enum

---
+++ Fistival!!!/Assets/@Scripts/Defines/MovementKeyStatus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Defines
+{
+    [Flags]
+    public enum MovementKeyStatus : sbyte
+    {
+        RIGHT = 1<<0,
+        LEFT = 1<<1,
+        UP = 1<<2,
+        DOWN= 1<<3,
+    }
+}
