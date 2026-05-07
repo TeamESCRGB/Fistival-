3fe96ac00c205c96f6a6a441dd43c46cd09f93ec
nuke1115 <nuclearbomb280@gmail.com>
2026-05-08 00:46:55 +0900

    feature: 가속도 계산 방식 수정

--- Fistival!!!/Assets/@Scripts/Coordinator/Movements/FlightMovementCoordinator.cs
+++ Fistival!!!/Assets/@Scripts/Coordinator/Movements/FlightMovementCoordinator.cs
@@ -1,23 +1,34 @@
-﻿using Defines;
+﻿using ComponentModule;
+using Defines;
 using InputHandler;
 using UnityEngine;
+using Utils;
 
 namespace Coordinator.Movements
 {
-    public class FlightMovementCoordinator :MonoBehaviour, IHorizontalMovementInputHandler, IVerticalMovementInputHandler
+    public class FlightMovementCoordinator :MonoBehaviour, IHorizontalMovementInputHandler, IVerticalMovementInputHandler, IPushable
     {
         [SerializeField]
         private LayerMask _groundLayer;
         private Rigidbody2D _parentRb2d;
         private float _speed;
-        private Vector2 _vel;
+        private Directions _nextXDir;
+        private Directions _nextYDir;
+        private MovementState _xMovState;
+        private MovementState _yMovState;
+
 
         private MovementKeyStatus _keyStatus = MovementKeyStatus.OFF;//0번비트:rightOn, 1번비트: leftOn, 2번비트: upOn, 3번비트:downOn
+        private const MovementKeyStatus _verticalMask = MovementKeyStatus.UP | MovementKeyStatus.DOWN;
+        private const MovementKeyStatus _horizontalMask = MovementKeyStatus.LEFT | MovementKeyStatus.RIGHT;
         public virtual void Init(float speed, Rigidbody2D parentRb2d)
         {
             _parentRb2d = parentRb2d;
             _speed = speed;
             _keyStatus = MovementKeyStatus.OFF;
+            _nextYDir = _nextXDir = Directions.OFF;
+            _yMovState = _xMovState = MovementState.OFF;
+            
         }
 
         private void FixedUpdate()
@@ -27,7 +38,8 @@ namespace Coordinator.Movements
                 return;
             }
 
-            _parentRb2d.linearVelocity = _vel;
+            _parentRb2d.linearVelocityX = MovementUtils.CalculateNewSpeed(_parentRb2d.linearVelocityX, _speed, (float)_nextXDir, ref _xMovState);
+            _parentRb2d.linearVelocityY = MovementUtils.CalculateNewSpeed(_parentRb2d.linearVelocityY, _speed, (float)_nextYDir, ref _yMovState);
         }
 
         public void PushTo(Vector2 force)
@@ -39,8 +51,12 @@ namespace Coordinator.Movements
         {
             if (pressed)
             {
+                if((_keyStatus & _horizontalMask) == MovementKeyStatus.OFF)
+                {
+                    _xMovState = MovementState.START_REQ;
+                }
                 _keyStatus |= MovementKeyStatus.LEFT;
-                _vel.x = -_speed;
+                _nextXDir = Directions.LEFT;
             }
             else
             {
@@ -53,8 +69,12 @@ namespace Coordinator.Movements
         {
             if (pressed)
             {
+                if ((_keyStatus & _horizontalMask) == MovementKeyStatus.OFF)
+                {
+                    _xMovState = MovementState.START_REQ;
+                }
                 _keyStatus |= MovementKeyStatus.RIGHT;
-                _vel.x = _speed;
+                _nextXDir = Directions.RIGHT;
             }
             else
             {
@@ -67,8 +87,12 @@ namespace Coordinator.Movements
         {
             if (pressed)
             {
+                if ((_keyStatus & _verticalMask) == MovementKeyStatus.OFF)
+                {
+                    _yMovState = MovementState.START_REQ;
+                }
                 _keyStatus |= MovementKeyStatus.DOWN;
-                _vel.y = -_speed;
+                _nextYDir = Directions.DOWN;
             }
             else
             {
@@ -81,8 +105,12 @@ namespace Coordinator.Movements
         {
             if (pressed)
             {
+                if ((_keyStatus & _verticalMask) == MovementKeyStatus.OFF)
+                {
+                    _yMovState = MovementState.START_REQ;
+                }
                 _keyStatus |= MovementKeyStatus.UP;
-                _vel.y = _speed;
+                _nextYDir = Directions.UP;
             }
             else
             {
@@ -93,29 +121,37 @@ namespace Coordinator.Movements
 
         private void RestoreHorizontalMovementState()
         {
-            _vel.x = 0;
+            _nextXDir = Directions.OFF;
 
             if ((_keyStatus & MovementKeyStatus.LEFT) == MovementKeyStatus.LEFT)
             {
-                _vel.x = -_speed;
+                _nextXDir = Directions.LEFT;
             }
             else if ((_keyStatus & MovementKeyStatus.RIGHT) == MovementKeyStatus.RIGHT)
             {
-                _vel.x = _speed;
+                _nextXDir = Directions.RIGHT;
+            }
+            else if ((_keyStatus & _horizontalMask) == MovementKeyStatus.OFF)
+            {
+                _xMovState = MovementState.STOP_REQ;
             }
         }
 
         private void RestoreVerticalMovementState()
         {
-            _vel.y = 0;
+            _nextYDir = Directions.OFF;
 
             if ((_keyStatus & MovementKeyStatus.DOWN) == MovementKeyStatus.DOWN)
             {
-                _vel.y = -_speed;
+                _nextYDir = Directions.DOWN;
             }
             else if ((_keyStatus & MovementKeyStatus.UP) == MovementKeyStatus.UP)
             {
-                _vel.y = _speed;
+                _nextYDir = Directions.UP;
+            }
+            else if ((_keyStatus & _verticalMask) == MovementKeyStatus.OFF)
+            {
+                _yMovState = MovementState.STOP_REQ;
             }
         }
     }
