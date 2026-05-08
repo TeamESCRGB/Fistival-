9454a46fd006ba3e04a5642b4fdc5aaac9e7a4ca
nuke1115 <nuclearbomb280@gmail.com>
2026-05-08 17:35:22 +0900

    add:IStunable추가

---
+++ Fistival!!!/Assets/@Scripts/Coordinator/Modes/IStunable.cs
@@ -0,0 +1,8 @@
+﻿namespace Coordinator.Movements
+{
+    public interface IStunable
+    {
+        public void StunFor(float time);
+        public void ReleaseStun();
+    }
+}
--- Fistival!!!/Assets/@Scripts/Coordinator/Movements/IInputLockable.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Coordinator.Movements
-{
-    public interface IInputLockable
-    {
-        public void LockInputFor(float time);
-        public void UnlockInput();
-    }
-}
