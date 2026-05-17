3a02b9e70f5775506a9f2c96c704489a01be8fda
nuke1115 <nuclearbomb280@gmail.com>
2026-05-17 15:39:53 +0900

    add:파동권 틀 추가

---
+++ Fistival!!!/Assets/@Scripts/Coordinator/Skills/hadouken.cs
@@ -0,0 +1,41 @@
+﻿using Coordinator;
+using Coordinator.Victims;
+using Defines;
+using Manager;
+using System;
+using UnityEngine;
+using Utils;
+
+namespace Assets._Scripts.Coordinator.Skills
+{
+    public class hadouken : SkillCoordinatorBase
+    {
+        public event Action<int> OnAttack;
+        [SerializeField]
+        private int _demendedCost = 0;
+        [SerializeField]
+        private int _projectileIDX;
+
+        public int GetDemendedCost()
+        {
+            return _demendedCost;
+        }
+
+        public void Attack(AttackStatus attackStatus, int objectDmg, Vector2 dir)
+        {
+
+
+        }
+
+        public override bool Act(IAttackable target, int calculatedDamage)
+        {
+            if (target.CanAttack())
+            {
+                target.TakeDamage(calculatedDamage);
+                target.StartInvincibleTime();
+            }
+            OnAttack?.Invoke(calculatedDamage);
+            return true;
+        }
+    }
+}
