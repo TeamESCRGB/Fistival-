827ce89244f12b6c64782255c2dea766406c69d4
nuke1115 <nuclearbomb280@gmail.com>
2026-05-20 19:07:19 +0900

    add: 메트로배니아모드 틀 추가

---
+++ Fistival!!!/Assets/@Scripts/Coordinator/Modes/MetroidvaniaMode.cs
@@ -0,0 +1,83 @@
+﻿using Coordinator.Movements;
+using Data;
+using Defines;
+using UnityEngine;
+using Utils;
+
+namespace Coordinator.Modes
+{
+    public class MetroidvaniaMode : ModeBase
+    {
+        private PlatformerMovementCoordinator _movCoordinator;
+        private float _objectWeight = 0;
+        public override ModeTypes ModeType => ModeTypes.METROIDVANIA;
+
+        protected override void OnAwake()
+        {
+            base.OnAwake();
+            _movCoordinator = gameObject.GetOrAddComponent<PlatformerMovementCoordinator>();
+        }
+
+        public override void Init(CommonModeData data)
+        {
+            base.Init(data);
+            _movCoordinator.Init(data.MoveSpeed, data.JumpPower, _commonData.SlownessSensitivity, _commonData.MaxSlowness, GetComponentInParent<Rigidbody2D>());
+            _inputCoordinator.SetJumpsMovementInputHandler(_movCoordinator);
+            _inputCoordinator.SetHorizontalMovementInputHandler(_movCoordinator);
+        }
+
+        public override void DeInit()
+        {
+
+            base.DeInit();
+        }
+
+        private void OnGrabbedObjectChanged(ObjectData objData)
+        {
+            if (objData == null)
+            {
+                _objectWeight = 0;
+            }
+            else
+            {
+                _objectWeight = objData.Weight;
+            }
+
+        }
+
+        private void OnChargeRateChanged(int now, int max)
+        {
+            _movCoordinator.SetSlowness(1 / (1 + (now / max * _objectWeight)));
+        }
+
+        public override void OnRMBEvent(bool pressed, Vector2 screenPos)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void OnLMBEvent(bool pressed, Vector2 screenPos)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void OnDropEvent(bool pressed)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void OnStunEnd()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void StunFor(float time)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void ReleaseStun()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
--- Fistival!!!/Assets/@Scripts/Defines/ModeTypes.cs
+++ Fistival!!!/Assets/@Scripts/Defines/ModeTypes.cs
@@ -8,6 +8,7 @@
         RHYTHM=3,
         SHOOT_2D=4,
         ROOT_SHOOTER=5,
-        WWE=6
+        WWE=6,
+        METROIDVANIA=7
     }
 }
