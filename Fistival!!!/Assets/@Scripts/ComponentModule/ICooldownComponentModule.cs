eb959119be993bd27d0688df61cf00d4b25ada8d
nuke1115 <nuclearbomb280@gmail.com>
2026-05-25 17:49:54 +0900

    refactor: CooldownComponentModule을 FixedUpdate와 Update로 분리

---
+++ Fistival!!!/Assets/@Scripts/ComponentModule/ICooldownComponentModule.cs
@@ -0,0 +1,14 @@
+﻿namespace ComponentModule
+{
+    public interface ICooldownComponentModuleBase
+    {
+        public void InitCooldown(float cooldownTime, int index, float timeChangedCallInterval);
+        public void DeinitCooldown();
+        public void SetCooldownTime(float time);
+        public void StartCooldown();
+        public void StopCooldown();
+        public bool IsCooldownEnded();
+        public float GetRemainedTime();
+        public void Tick(float dt);
+    }
+}
