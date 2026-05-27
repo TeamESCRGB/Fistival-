e229022c4be408a3f9042921caff0b57434017e2
nuke1115 <nuclearbomb280@gmail.com>
2026-05-27 20:50:14 +0900

    fix: 하이라이터 활성화 방식 변경

--- Fistival!!!/Assets/@Scripts/Coordinator/TargetObjectHighlighter.cs
+++ Fistival!!!/Assets/@Scripts/Coordinator/TargetObjectHighlighter.cs
@@ -1,25 +1,15 @@
-using UnityEngine;
-
 namespace Coordinator
 {
     public class TargetObjectHighlighter : HIghlighterBase
     {
-        [SerializeField]
-        private LayerMask _targeterLayer;
-        private void OnTriggerEnter2D(Collider2D collision)
+        public void ActivateShader()
         {
-            if(((1 << collision.gameObject.layer) & _targeterLayer) != 0)
-            {
-                _renderer.sharedMaterial = _on;
-            }
+            _renderer.sharedMaterial = _on;
         }
 
-        private void OnTriggerExit2D(Collider2D collision)
+        public void DeActivateShader()
         {
-            if (((1 << collision.gameObject.layer) & _targeterLayer) != 0)
-            {
-                _renderer.sharedMaterial = _off;
-            }
+            _renderer.sharedMaterial = _off;
         }
     }
 }
