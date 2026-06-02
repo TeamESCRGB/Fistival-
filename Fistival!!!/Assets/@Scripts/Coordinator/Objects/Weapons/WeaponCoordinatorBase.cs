f7b610159c78cfb5024705c662d9a502d27f263a
nuke1115 <nuclearbomb280@gmail.com>
2026-06-02 20:34:50 +0900

    feature: 무기 종류의 베이스 클래스 추가

---
+++ Fistival!!!/Assets/@Scripts/Coordinator/Objects/Weapons/WeaponCoordinatorBase.cs
@@ -0,0 +1,35 @@
+using Data;
+using UnityEngine;
+
+namespace Coordinator.Objects.Weapons
+{
+    public abstract class WeaponCoordinatorBase : ObjectCoordinator
+    {
+        [SerializeField]
+        protected int _maxWeaponUseCnt = 3;
+        protected int _weaponUseCnt = 3;
+        [SerializeField]
+        protected string _animName;
+
+
+        public override void Init(ObjectData data)
+        {
+            base.Init(data);
+            _weaponUseCnt = _maxWeaponUseCnt;
+        }
+
+        public bool CanUseWeapon()
+        {
+            return _weaponUseCnt > 0;
+        }
+
+        public string GetAnimKey()
+        {
+            return _animName;
+        }
+
+        public abstract void OnLMBReleased();
+
+        public abstract void OnLMBPressed();
+    }
+}
