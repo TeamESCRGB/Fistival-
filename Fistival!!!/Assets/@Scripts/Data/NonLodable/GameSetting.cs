a7b4cba14bacfd229b3de7ac31a55a40106ba4a2
nuke1115 <nuclearbomb280@gmail.com>
2026-06-05 22:28:24 +0900

    feature: 데이터 포맷 제작

---
+++ Fistival!!!/Assets/@Scripts/Data/NonLodable/GameSetting.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Data.NonLodable
+{
+    public class GameSetting
+    {
+        public float Volume { get; set; } = 1;
+    }
+}
---
+++ Fistival!!!/Assets/@Scripts/Data/NonLodable/PlayerSaveFile.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Data.NonLodable
+{
+    public class PlayerSaveFile
+    {
+        public int Money { get; set; } = 0;
+        public List<int> PurchasedItems { get; set; } = new List<int>();
+        public int[] EquippedItems { get; set; } = new int[3];
+        public List<int> UnlockedTrainingStage { get; set; } = new List<int>();
+    }
+}
