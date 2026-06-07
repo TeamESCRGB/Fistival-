62d5ddb8f069dff9df889bbbe198b90c00e14638
nuke1115 <nuclearbomb280@gmail.com>
2026-06-07 15:48:57 +0900

    add: 씬 파일들 추가

--- Fistival!!!/Assets/@Scripts/Defines/SceneType.cs
+++ Fistival!!!/Assets/@Scripts/Defines/SceneType.cs
@@ -1,10 +1,12 @@
-using UnityEngine;
-
 namespace Defines
 {
     public enum SceneType
     {
         UNKNOWN = -666775,
-        Test=-1
+        Test=-1,
+        TitleScene=1,
+        MainScene=2,
+        LobbyScene=3,
+        GameScene=4
     }
 }
