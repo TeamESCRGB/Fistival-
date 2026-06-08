fa8745a80ac757454d4138bc21fe7f7f281a2905
nuke1115 <nuclearbomb280@gmail.com>
2026-06-08 15:41:30 +0900

    feature: 볼륨 설정 기능이 있는 세팅메뉴 추가

---
+++ Fistival!!!/Assets/@Scripts/UI/Popup/SettingMenu.cs
@@ -0,0 +1,58 @@
+using Manager;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using Utils;
+
+namespace UI.Popup
+{
+    public class SettingMenu : UIPopupBase
+    {
+        enum Sliders
+        {
+            VolumeSlider
+        }
+        enum Texts
+        {
+            SettingVolumeValueText
+        }
+        enum Buttons
+        {
+            SettingExitButton
+        }
+        public override bool Init()
+        {
+            if(base.Init() == false)
+            {
+                return false;
+            }
+
+            BindText(typeof(Texts));
+            Bind<Slider>(typeof(Sliders));
+            BindButton(typeof(Buttons));
+
+            GetButton((int)Buttons.SettingExitButton).gameObject.BindUIEvent(OnExit);
+            Get<Slider>((int)Sliders.VolumeSlider).onValueChanged.AddListener(OnSliderValueChanged);
+            Get<Slider>((int)Sliders.VolumeSlider).value = Managers.Instance.GameManager.Volume;
+            
+            return true;
+        }
+
+        private void OnExit(PointerEventData data)
+        {
+            Managers.Instance.UIManager.ClosePopupUI();;
+        }
+
+        private void OnSliderValueChanged(float value)
+        {
+            int volume = Mathf.FloorToInt(value * 100);
+            GetText((int)Texts.SettingVolumeValueText).text = volume.ToString();
+            Managers.Instance.GameManager.Volume = value;
+        }
+
+        private void OnDisable()
+        {
+            Managers.Instance.SaveDataManager.SaveSettings();
+        }
+    }
+}
