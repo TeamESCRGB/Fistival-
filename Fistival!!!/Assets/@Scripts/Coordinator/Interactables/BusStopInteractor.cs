6caf3adfd2a9d11aa12b13331394bcaa534f3b4e
nuke1115 <nuclearbomb280@gmail.com>
2026-06-13 15:33:38 +0900

    feature: 상호작용 클래스 추가

---
+++ Fistival!!!/Assets/@Scripts/Coordinator/Interactables/BusStopInteractor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace Coordinator.Interactables
+{
+    public class BusStopInteractor : InteractableObjectCoordinator
+    {
+        public override void Interact()
+        {
+            Debug.Log("busstop interact");
+        }
+    }
+}
---
+++ Fistival!!!/Assets/@Scripts/Coordinator/Interactables/ShopInteractor.cs
@@ -0,0 +1,13 @@
+﻿using Coordinator;
+using UnityEngine;
+
+namespace Coordinator.Interactables
+{
+    public class ShopInteractor : InteractableObjectCoordinator
+    {
+        public override void Interact()
+        {
+            Debug.Log("Shop Interact");
+        }
+    }
+}
