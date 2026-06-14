8e3fd9b622b4a98faab3f28bb91d727ce35a3905
nuke1115 <nuclearbomb280@gmail.com>
2026-06-14 13:26:13 +0900

    add: CommonModeData에서 PlayerData복제

---
+++ Fistival!!!/Assets/@Scripts/Data/DataLoaders/PlayerDataLoader.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace Data.DataLoaders
+{
+    public class PlayerDataLoader : ILoader<int,PlayerData>
+    {
+        public PlayerData playerData;
+        public Dictionary<int, PlayerData> MakeDict()
+        {
+            Dictionary<int, PlayerData> dict = new Dictionary<int, PlayerData>();
+
+            dict.Add(0, playerData);
+
+            return dict;
+        }
+
+        public PlayerData GetData()
+        {
+            return playerData;
+        }
+    }
+}
