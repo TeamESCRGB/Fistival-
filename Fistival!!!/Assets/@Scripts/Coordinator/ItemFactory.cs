d834302f5d969cd20d3c556740eef313e96ef13d
nuke1115 <nuclearbomb280@gmail.com>
2026-06-15 20:12:29 +0900

    add: 아이템 클래스를 생성하고 관리하는 ItemFactory추가

---
+++ Fistival!!!/Assets/@Scripts/Coordinator/ItemFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Coordinator
+{
+    public class ItemFactory
+    {
+        private List<ItemBase> _items = new List<ItemBase>();
+
+        public ItemFactory()
+        {
+
+        }
+
+        public ItemBase GetItem(int idx)
+        {
+            if(idx < 0 || idx >= _items.Count)
+            {
+                return null;
+            }
+            return _items[idx];
+        }
+    }
+}
