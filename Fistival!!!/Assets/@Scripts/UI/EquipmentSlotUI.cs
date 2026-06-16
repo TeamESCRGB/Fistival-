c724ad53b11f7174534a72a321f9d0196ffc49ba
nuke1115 <nuclearbomb280@gmail.com>
2026-06-16 15:13:21 +0900

    feature: Item및 Equipment의 슬롯 UI추가

---
+++ Fistival!!!/Assets/@Scripts/UI/EquipmentSlotUI.cs
@@ -0,0 +1,24 @@
+﻿using Data;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class EquipmentSlotUI : ItemSlotUI
+    {
+
+        public override void SetItem(ItemData data)
+        {
+            if (data is null)
+            {
+                _data = null;
+                gameObject.SetActive(false);
+                return;
+            }
+
+            gameObject.SetActive(true);
+            
+            _data = data;
+        }
+    }
+}
