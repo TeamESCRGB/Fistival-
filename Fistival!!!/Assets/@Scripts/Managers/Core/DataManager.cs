f5415ee527efc0ae78c44a8b01d6a07aeb69fa87
nuke1115 <nuclearbomb280@gmail.com>
2026-06-19 23:42:59 +0900

    add: CollectionData추가

--- Fistival!!!/Assets/@Scripts/Managers/Core/DataManager.cs
+++ Fistival!!!/Assets/@Scripts/Managers/Core/DataManager.cs
@@ -20,6 +20,7 @@ namespace Manager.Core
         public Dictionary<int,StageData> StageDataDict { get; private set; }
         public Dictionary<int,ItemData> ItemDataDIct { get; private set; }
         public PlayerData PlayerData { get; private set; }
+        public Dictionary<int,CollectionData> CollectionDataDict { get; private set; }
 
         public void Init()
         {
@@ -30,6 +31,7 @@ namespace Manager.Core
             StageDataDict = LoadJson<StageDataLoader, int, StageData>("StageData").MakeDict();
             ItemDataDIct = LoadJson<ItemDataLoader, int, ItemData>("ItemData").MakeDict();
             PlayerData = LoadJson<PlayerDataLoader, int, PlayerData>("PlayerData").GetData();
+            CollectionDataDict = LoadJson<CollectionDataLoader, int, CollectionData>("CollectionData").MakeDict();
         }
 
         //솔직히, 이게 어떻게 가능한건지 아직 모르겠다
