92dc83650edd4fd29268ebe0956ee5f04ba6db69
nuke1115 <nuclearbomb280@gmail.com>
2026-06-19 21:24:46 +0900

    add: StartStage추가

--- Fistival!!!/Assets/@Scripts/Managers/Contents/StageManager.cs
+++ Fistival!!!/Assets/@Scripts/Managers/Contents/StageManager.cs
@@ -8,21 +8,32 @@ namespace Manager.Contents
         private int _stageIdx = -1;
         private StageData _data;
 
+        private double _scaledTimeStart = 0;
+        private double _unscaledTimeStart = 0;
+        private int _collectedMoney = 0;
+
         public void Init()
         {
             _stageIdx = -1;
             _data = null;
-        }
-
-        public void ClearPlayDatas()
-        {
-
+            _scaledTimeStart = 0;
+            _unscaledTimeStart = 0;
+            _collectedMoney = 0;
         }
 
         public void TrySyncToPlayerData()
         {
 
         }
+
+
+        public void StartStage()
+        {
+            _scaledTimeStart = _unscaledTimeStart = Time.timeAsDouble;
+
+
+        }
+
 
         public StageData GetStageData()
         {
