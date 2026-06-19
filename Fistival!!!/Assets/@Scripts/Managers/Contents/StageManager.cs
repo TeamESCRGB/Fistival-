87f36273713214fd2edf9bcebbe97f00dc46a69d
nuke1115 <nuclearbomb280@gmail.com>
2026-06-19 20:44:14 +0900

    feature: StageManager에 SetStageIDX, GetStageData추가

--- Fistival!!!/Assets/@Scripts/Managers/Contents/StageManager.cs
+++ Fistival!!!/Assets/@Scripts/Managers/Contents/StageManager.cs
@@ -1,12 +1,17 @@
+using Data;
 using UnityEngine;
 
 namespace Manager.Contents
 {
     public class StageManager
     {
+        private int _stageIdx = -1;
+        private StageData _data;
+
         public void Init()
         {
-
+            _stageIdx = -1;
+            _data = null;
         }
 
         public void ClearPlayDatas()
@@ -17,6 +22,17 @@ namespace Manager.Contents
         public void TrySyncToPlayerData()
         {
 
+        }
+
+        public StageData GetStageData()
+        {
+            return _data;
+        }
+
+        public void SetStageIDX(int idx)
+        {
+            _stageIdx = idx;
+            _data = Managers.Instance.DataManager.StageDataDict[idx];
         }
     }
 }
