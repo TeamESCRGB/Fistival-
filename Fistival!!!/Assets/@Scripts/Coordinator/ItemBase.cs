f20253b94c0052881f452580bc32069fe1c02cc8
nuke1115 <nuclearbomb280@gmail.com>
2026-06-20 15:32:57 +0900

    feature: ItemBase추가

---
+++ Fistival!!!/Assets/@Scripts/Coordinator/ItemBase.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Coordinator
+{
+    public abstract class ItemBase : MonoBehaviour
+    {
+        [SerializeField]
+        protected LayerMask _activateTargetLayer;
+        protected abstract void InternalCollisionHandler(Collider2D collision);
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            InternalCollisionHandler(collision);
+        }
+    }
+}
