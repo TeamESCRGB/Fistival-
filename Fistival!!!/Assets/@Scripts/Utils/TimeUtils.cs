607ff671a04da80b3a6d1d9ba4c162606cd71971
nuke1115 <nuclearbomb280@gmail.com>
2026-06-22 02:19:27 +0900

    fix: 0이하면 0리턴하도록 SecToTime수정

--- Fistival!!!/Assets/@Scripts/Utils/TimeUtils.cs
+++ Fistival!!!/Assets/@Scripts/Utils/TimeUtils.cs
@@ -8,6 +8,10 @@ namespace Utils
     {
         public static string SecToTimeStr(double sec)
         {
+            if(sec<0)
+            {
+                return "00:00:00";
+            }
             TimeSpan t = TimeSpan.FromSeconds(sec);
             return t.TotalDays < 1 ? t.ToString(@"hh\:mm\:ss") : t.ToString(@"dd\:hh\:mm\:ss");
         }
