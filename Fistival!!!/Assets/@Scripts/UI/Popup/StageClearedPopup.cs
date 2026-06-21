afe1e9af09c20e7964ecbba612c37e0b43546115
nuke1115 <nuclearbomb280@gmail.com>
2026-06-22 01:47:52 +0900

    feature: 스테이지 세이브 기능 추가

--- Fistival!!!/Assets/@Scripts/UI/Popup/StageClearedPopup.cs
+++ Fistival!!!/Assets/@Scripts/UI/Popup/StageClearedPopup.cs
@@ -2,6 +2,7 @@ using Manager;
 using System;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using Utils;
 
 namespace UI.Popup
@@ -27,6 +28,11 @@ namespace UI.Popup
             ClearTimeCounter
         }
 
+        enum Buttons
+        {
+            Next
+        }
+
         private int _idx=0;
 
         public override bool Init()
@@ -36,9 +42,11 @@ namespace UI.Popup
                 return false;
             }
 
+            BindButton(typeof(Buttons));
             BindImage(typeof(Images));
             BindText(typeof(Texts));
 
+            GetButton((int)Buttons.Next).gameObject.BindUIEvent(OnNext);
             StartCoroutine(ShowRoutine());
 
             return true;
@@ -68,11 +76,25 @@ namespace UI.Popup
             GetText((int)Texts.ClearTimeCounter).text = TimeUtils.SecToTimeStr(stageData.ClearTimeWithPause);
             yield return waiter;
             GetText((int)Texts.TotalDamageCounter).text = stageData.TotalGainedDamage.ToString("N0");
-
-
         }
 
 
+        private void OnNext(PointerEventData _)
+        {
+            Managers.Instance.UIManager.ShowPopupUI<BasicConfirmBox>("BasicConfirmBox").SetCallback(OnSaveYes, OnSaveNo).SetText("현 시점의 세이브를 저장하시겠습니까?");
+        }
 
+        private void OnSaveYes()
+        {
+            Managers.Instance.UIManager.ClosePopupUI();
+            Managers.Instance.SaveDataManager.SaveSaveData();
+            Managers.Instance.StageManager.ReturnToLobby();
+        }
+
+        private void OnSaveNo()
+        {
+            Managers.Instance.UIManager.ClosePopupUI();
+            Managers.Instance.StageManager.ReturnToLobby();
+        }
     }
 }
