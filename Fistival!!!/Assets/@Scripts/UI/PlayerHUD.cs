827443898534d69ac72bc4be010c21149f50eb62
nuke1115 <nuclearbomb280@gmail.com>
2026-06-24 22:29:34 +0900

    feature: PlayerHUD기본 스크립트 틀 추가

---
+++ Fistival!!!/Assets/@Scripts/UI/PlayerHUD.cs
@@ -0,0 +1,87 @@
+using Coordinator;
+using Coordinator.Objects;
+using Data;
+using Manager.Contents;
+using System.Collections;
+using UnityEngine;
+
+namespace UI
+{
+    public class PlayerHUD : UIBase
+    {
+        enum Images
+        {
+            Heart1=0,
+            Heart2=1,
+            Heart3=2,
+            Heart4=3,
+            Heart5=4,
+            Heart6=5,
+            Heart7=6,
+            Heart8=7,
+            Charge1=8,
+            Charge2=9,
+            Charge3=10,
+            Charge4=11
+        }
+
+        enum Objects
+        {
+            BossHPBarHUD
+        }
+
+        private const int _chargeOffset = 8;
+
+        public override bool Init()
+        {
+            if(base.Init() == false)
+            {
+                return false;
+            }
+            BindObject(typeof(Objects));
+            BindImage(typeof(Images));
+
+
+            var playerCoord = FindAnyObjectByType<PlayerCoordinator>();
+            var modeManageCoord = FindAnyObjectByType<ModeManageCoordinator>();
+            var playerData = playerCoord.GetPlayerData();
+            modeManageCoord.OnModeChanged += OnModeChanged;
+            playerCoord.GetComponentInChildren<HPCoordinator>().SubscribeOnHPChanged(OnHPChanged);
+            GetObject((int)Objects.BossHPBarHUD).SetActive(false);
+            OnModeChanged(modeManageCoord.GetNowMode());
+
+            for(int i = 0; i < 8; i++)
+            {
+                GetImage(i).gameObject.SetActive(i < playerData.MaxChargeCnt);
+            }
+
+            for(int i = _chargeOffset; i < 12; i++)
+            {
+                GetImage(i).gameObject.SetActive(i < playerData.MaxHP + _chargeOffset);
+            }
+
+            return true;
+        }
+
+        private void OnModeChanged(ModeBase mode)
+        {
+            mode.GetComponentInChildren<HandCoordinatorBase>().OnChargeRateChanged += OnChargeRateChanged;
+            mode.GetComponentInChildren<HandCoordinatorBase>().OnGrabbedObjectChanged += OnGrabbedObjectChanged;
+        }
+
+        private void OnHPChanged(int old, int now, int delta)
+        {
+
+        }
+
+        private void OnChargeRateChanged(int now, int max)
+        {
+
+        }
+
+        private void OnGrabbedObjectChanged(ObjectData obj)
+        {
+
+        }
+    }
+}
