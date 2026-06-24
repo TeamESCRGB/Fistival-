ac3a8355d32ad037b64b5758b099dc395cc12a4f
nuke1115 <nuclearbomb280@gmail.com>
2026-06-24 15:11:09 +0900

    feature:체크포인트 오브젝트 기능구현 추가

---
+++ Fistival!!!/Assets/@Scripts/Coordinator/Victims/CheckpointActivator.cs
@@ -0,0 +1,46 @@
+﻿using Coordinator.Interactables;
+using UnityEngine;
+
+namespace Coordinator.Victims
+{
+    public class CheckpointActivator : MonoBehaviour, IAttackable
+    {
+        private int _maskedLayer = 0;
+        private CheckpointInteractor _checkpoint;
+        private void Start()
+        {
+            _checkpoint = transform.parent.GetComponent<CheckpointInteractor>();
+            _maskedLayer = 1 << gameObject.layer;
+        }
+
+        public void TakeDamage(int damage)
+        {
+            _checkpoint.CheckCheckpoint(true);
+        }
+
+        public int GetMaskedLayer()
+        {
+            return _maskedLayer;
+        }
+        public bool CanAttack()
+        {
+            return _checkpoint.IsCheckpointChecked() == false;
+        }
+        #region UnusedFuncs
+        public T RequestComponent<T>() where T : class
+        {
+            return GetComponent<T>();
+        }
+        
+        public void StartInvincibleTime()
+        {
+
+        }
+
+        public void TakeKnockBack(Vector2 force)
+        {
+
+        }
+        #endregion
+    }
+}
