97726e0627a9f7dc1d236de1469a8b2dc032abc2
nuke1115 <nuclearbomb280@gmail.com>
2026-06-28 22:36:05 +0900

    feature: PlatformerPatrolMob추가

---
+++ Fistival!!!/Assets/@Scripts/Coordinator/Mobs/PlatformerPatrolMob.cs
@@ -0,0 +1,68 @@
+using ComponentModule;
+using Coordinator.Movements;
+using Coordinator.TriggerMovement;
+using Data;
+using Defines;
+using Manager;
+using UnityEngine;
+
+namespace Coordinator.Mobs
+{
+    public class PlatformerPatrolMob : MobCoordinatorBase
+    {
+        [SerializeField]
+        protected MovementKeyStatus _initialDir;
+        protected PlatfoermerTriggerMovementCoordinator _move;
+        
+
+        protected override void OnAwake()
+        {
+            base.OnAwake();
+            _move = GetComponentInChildren<PlatfoermerTriggerMovementCoordinator>();
+        }
+
+        public override void Init(CommonMobData data)
+        {
+            base.Init(data);
+            _move.Init(data.Speed, 0, GetComponent<Rigidbody2D>(), _initialDir);
+        }
+
+        public override void StunFor(float time)
+        {
+            if (time <= 0 || _stunCounter.GetRemainedTime() >= time)
+            {
+                return;
+            }
+
+            if (_stunCounter.IsCooldownEnded())
+            {
+                _movLock.LockMovement();
+            }
+
+            _stunCounter.SetCooldownTime(time);
+            _stunCounter.StartCooldown();
+        }
+
+        public override void ReleaseStun()
+        {
+            if (_stunCounter is null || _stunCounter.IsCooldownEnded())
+            {
+                return;
+            }
+            _stunCounter.StopCooldown();
+        }
+
+        public override void OnStunEnd()
+        {
+            _movLock.UnlockMovement();
+        }
+
+        #region Unused
+        protected override void OnAggroStateChanged(bool isAggro, Collider2D collider)
+        {
+
+        }
+        #endregion
+
+    }
+}
