85516451aca7a6a4a9d64354222b045fe8d8de24
nuke1115 <nuclearbomb280@gmail.com>
2026-06-29 00:45:39 +0900

    feature: 플레이어 추적하는 몹 클래스 추가

---
+++ Fistival!!!/Assets/@Scripts/Coordinator/Mobs/ChasePlatformerPatrolMob.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace Coordinator.Mobs
+{
+    public class ChasePlatformerPatrolMob : PlatformerPatrolMob
+    {
+        [SerializeField]
+        private float _aggroSpeed;
+
+
+        protected override void OnAggroStateChanged(bool isAggro, Collider2D collider)
+        {
+            if(isAggro)
+            {
+                _move.FollowTarget(_aggroSpeed,collider.gameObject.transform);
+            }
+            else
+            {
+                _move.StopFollow();
+            }
+        }
+    }
+}
