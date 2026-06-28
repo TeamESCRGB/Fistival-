eaf20bb5bd640bc4f317f8e8ef2ecd7a6a1b645b
nuke1115 <nuclearbomb280@gmail.com>
2026-06-28 20:54:12 +0900

    feature: Init과 follow추가

--- Fistival!!!/Assets/@Scripts/Coordinator/Movements/TriggerMovement/PlatfoermerTriggerMovementCoordinator.cs
+++ Fistival!!!/Assets/@Scripts/Coordinator/Movements/TriggerMovement/PlatfoermerTriggerMovementCoordinator.cs
@@ -5,15 +5,66 @@ namespace Coordinator.TriggerMovement
 {
     public class PlatfoermerTriggerMovementCoordinator : MonoBehaviour
     {
+        private float _speed;
+        private Transform _target;
+        private bool _isFollowOn = false;
         private PlatformerMovementCoordinator _platformerMov;
         private void Awake()
         {
             _platformerMov = GetComponent<PlatformerMovementCoordinator>();
         }
 
+        public void Init(float speed,  float jumpPower, Rigidbody2D rb2d)
+        {
+            _platformerMov.Init(speed, jumpPower, 0, 1, rb2d);
+            _target = null;
+            _isFollowOn = false;
+            _speed = speed;
+        }
+
+        public void FollowTarget(float speed, Transform target)
+        {
+            _isFollowOn=true;
+            _target = target;
+            _platformerMov.SetMaxSpeed(speed);
+        }
+
+        public void StopFollow()
+        {
+            _isFollowOn=false;
+            _platformerMov.SetMaxSpeed(_speed);
+            _target = null;
+        }
+
+        private void Update()
+        {
+            if(_isFollowOn)
+            {
+                if(_target == null)
+                {
+                    return;
+                }
+                if(_target.position.x < transform.position.x)
+                {
+                    _platformerMov.OnRightMovementInputEvent(false);
+                    _platformerMov.OnLeftMovementInputEvent(true);
+                }
+                else
+                {
+                    _platformerMov.OnLeftMovementInputEvent(false);
+                    _platformerMov.OnRightMovementInputEvent(true);
+                }
+            }
+        }
+
         private void OnTriggerEnter2D(Collider2D collision)
         {
             if (collision.gameObject == null)
+            {
+                return;
+            }
+
+            if (_isFollowOn)
             {
                 return;
             }
@@ -28,11 +79,11 @@ namespace Coordinator.TriggerMovement
                 _platformerMov.OnRightMovementInputEvent(false);
                 _platformerMov.OnLeftMovementInputEvent(true);
             }
-            else if(collision.CompareTag("JumpTag"))
+            else if (collision.CompareTag("JumpTag"))
             {
                 _platformerMov.OnJumpMovementInputEvent(true);
             }
-            else if(collision.CompareTag("DownJumpTag"))
+            else if (collision.CompareTag("DownJumpTag"))
             {
                 _platformerMov.OnDownJumpMovementInputEvent(true);
             }
