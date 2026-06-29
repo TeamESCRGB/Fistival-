230cff2028e02875c03e256024da5fecc6a06863
nuke1115 <nuclearbomb280@gmail.com>
2026-06-30 01:05:09 +0900

    feature: MobActBase에 내부에 루틴 저장 및 실행 중지 추가

--- Fistival!!!/Assets/@Scripts/Coordinator/MobActBase.cs
+++ Fistival!!!/Assets/@Scripts/Coordinator/MobActBase.cs
@@ -1,16 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using UnityEngine;
 
 namespace Coordinator
 {
-    public abstract class MobActBase
+    public abstract class MobActBase : MonoBehaviour
     {
-        public Action _onActEnd;
+        protected Action _onActEnd;
+        protected Coroutine _routine;
         public virtual void Init(Action onActionEnd)
         {
             _onActEnd = onActionEnd;
+            _routine = null;
         }
+
+        public abstract void StopAct();
+
         public abstract void Act();
     }
 }
