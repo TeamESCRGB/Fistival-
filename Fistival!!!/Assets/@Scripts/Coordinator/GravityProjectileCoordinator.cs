5063142e82cbf318f8759c65a34649691921d211
nuke1115 <nuclearbomb280@gmail.com>
2026-06-29 19:02:17 +0900

    feature: 특정 위치로 발사하는 중력 영향 받는 투사체 추가

---
+++ Fistival!!!/Assets/@Scripts/Coordinator/GravityProjectileCoordinator.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using static Utils.MovementUtils;
+
+namespace Coordinator
+{
+    public class GravityProjectileCoordinator : ProjectileCoordinator
+    {
+        private Vector3 _pos;
+
+        public virtual void Launch(Vector3 initialPos, Vector3 pos)
+        {
+            _pos = pos;
+            transform.position = initialPos;
+            Vector2 impulseForce = PreciseCalculateThrowPower(_pos - transform.position, _baseSpeed, _rb2d.linearDamping, 0.001f, Mathf.Abs(Physics2D.gravity.y), _rb2d.linearVelocity);
+            _rb2d.AddForce(impulseForce, ForceMode2D.Impulse);
+        }
+
+        protected override void OnFixedUpdate()
+        {
+            base.OnFixedUpdate();
+            _projActor.LookDir(_rb2d.linearVelocity);
+        }
+    }
+}
