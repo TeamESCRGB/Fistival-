7803a8febabb9321b783c2ae51661221185b619a
nuke1115 <nuclearbomb280@gmail.com>
2026-06-30 01:06:27 +0900

    feature: 점프하며 이동하는 행동을 위한 Act추가

---
+++ Fistival!!!/Assets/@Scripts/Coordinator/MobActs/JumpMovementAct.cs
@@ -0,0 +1,83 @@
+using Coordinator.Movements;
+using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Coordinator.MobActs
+{
+    public class JumpMovementAct : MobActBase
+    {
+        private Rigidbody2D _rb2d;
+        private float _maxSpeed=0;
+        private PlatformerMovementCoordinator _mov;
+        private Transform _player;
+        private WaitForSeconds _jumpDelay;
+
+        private bool _isMoving;
+
+        public void Init(Action onEnd, PlatformerMovementCoordinator movCoord, Rigidbody2D rb2d, float maxSpeed,float jumpDelay)
+        {
+            Init(onEnd);
+            _maxSpeed = maxSpeed;
+            _rb2d = rb2d;
+            _mov = movCoord;
+            _player = FindAnyObjectByType<PlayerCoordinator>().transform;
+            _jumpDelay = new WaitForSeconds(jumpDelay);
+            _isMoving = false;
+        }
+
+        private IEnumerator ActRoutine()
+        {
+            _mov.SetMaxSpeed(_maxSpeed);
+            if (_player.position.x < transform.position.x)
+            {
+                _mov.OnLeftMovementInputEvent(true);
+            }
+            else
+            {
+                _mov.OnRightMovementInputEvent(true);
+            }
+
+            yield return _jumpDelay;
+            _mov.SetMaxSpeed(0);
+            _mov.OnLeftMovementInputEvent(false);
+            _mov.OnRightMovementInputEvent(false);
+            _mov.OnJumpMovementInputEvent(true);
+            _isMoving = true;
+        }
+
+        private void FixedUpdate()
+        {
+            if(_isMoving == false)
+            {
+                return;
+            }
+
+            if(_rb2d.linearVelocity.magnitude < 0.0001f)
+            {
+                End();
+            }
+        }
+
+        private void End()
+        {
+            _rb2d.linearVelocity = Vector2.zero;
+            _isMoving = false;
+            _onActEnd?.Invoke();
+        }
+
+        public override void Act()
+        {
+            _routine = StartCoroutine(ActRoutine());
+        }
+
+        public override void StopAct()
+        {
+            if (_routine != null)
+            {
+                StopCoroutine(_routine);
+            }
+            End();
+        }
+    }
+}
