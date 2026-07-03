2bb8f8b0d70b1b84cc08c7021b40b8dae784d169
nuke1115 <nuclearbomb280@gmail.com>
2026-07-04 00:46:40 +0900

    feature: 중간보스1 기능 틀과 프리펩 제작

---
+++ Fistival!!!/Assets/@Scripts/Coordinator/Mobs/SotCopMob.cs
@@ -0,0 +1,131 @@
+﻿using Coordinator.MobActs;
+using Coordinator.Movements;
+using Coordinator.Skills;
+using Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace Coordinator.Mobs
+{
+    public class SotCopMob : MobCoordinatorBase
+    {
+        [SerializeField]
+        protected int _damage;
+        [SerializeField]
+        protected float _stunTime;
+        [SerializeField]
+        protected float _knockbackForce;
+        [SerializeField]
+        protected float _jumpPow;
+
+        [SerializeField]
+        private int _slashProjectileIdx;
+        [SerializeField]
+        protected int _slashCnt;
+
+        protected PlatformerMovementCoordinator _move;
+
+        private MobActBase[] _acts = new MobActBase[1];
+
+
+
+        protected Transform _player;
+        protected bool _isActing;
+        protected float _skillTime;
+
+        protected override void OnAwake()
+        {
+            base.OnAwake();
+            _move = GetComponentInChildren<PlatformerMovementCoordinator>();
+            _acts[0] = GetComponent<ProjectileLaunchAct>();
+            //_acts[1] = GetComponent<ProjectileLaunchAct>();
+            //_acts[2] = GetComponent<ProjectileLaunchAct>();
+        }
+
+        public override void Init(CommonMobData data)
+        {
+            base.Init(data);
+            _isActing = false;
+            _skillTime = _skillDelay;
+            _player = FindAnyObjectByType<PlayerCoordinator>().transform;
+            GetComponentInChildren<TouchDamageSkill>().Init(data.PlayerHitboxLayer, _damage, _stunTime, _knockbackForce);
+            _move.Init(data.Speed, _jumpPow, 1, 1, GetComponent<Rigidbody2D>());
+            ((ProjectileLaunchAct)_acts[0]).Init(() => { _isActing = false; }, _animator, _slashProjectileIdx, _slashCnt, data.PlayerHitboxLayer);
+        }
+
+        private void Update()
+        {
+            if (_isActing)
+            {
+                return;
+            }
+
+            if (_skillTime < _skillDelay)
+            {
+                _skillTime += Time.deltaTime;
+                return;
+            }
+
+            if (_stunCounter.IsCooldownEnded() == false)
+            {
+                return;
+            }
+
+            _skillTime = 0;
+            _isActing = true;
+
+            if(_player.position.x < transform.position.x)
+            {
+                _move.OnLeftMovementInputEvent(true);
+                _move.OnLeftMovementInputEvent(false);
+            }
+            else
+            {
+                _move.OnRightMovementInputEvent(true);
+                _move.OnRightMovementInputEvent(false);
+            }
+
+            _acts[UnityEngine.Random.Range(0, _acts.Length)].Act();
+
+        }
+
+
+
+        public override void StunFor(float time)
+        {
+            if (time <= 0 || _stunCounter.GetRemainedTime() >= time)
+            {
+                return;
+            }
+
+            if (_stunCounter.IsCooldownEnded())
+            {
+                _movLock.LockMovement();
+            }
+
+            _stunCounter.SetCooldownTime(time);
+            _stunCounter.StartCooldown();
+        }
+
+        public override void ReleaseStun()
+        {
+            if (_stunCounter is null || _stunCounter.IsCooldownEnded())
+            {
+                return;
+            }
+            _stunCounter.StopCooldown();
+        }
+
+        public override void OnStunEnd()
+        {
+            _movLock.UnlockMovement();
+        }
+
+        protected override void OnAggroStateChanged(bool isAggroOn, Collider2D player)
+        {
+            
+        }
+    }
+}
