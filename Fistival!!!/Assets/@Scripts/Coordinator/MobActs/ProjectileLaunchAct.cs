70269159a140d62acf5355327a4e8bd8dc7ecc62
nuke1115 <nuclearbomb280@gmail.com>
2026-07-04 00:44:37 +0900

    feature: 일반 탄막 발사하는 행동 추가

---
+++ Fistival!!!/Assets/@Scripts/Coordinator/MobActs/ProjectileLaunchAct.cs
@@ -0,0 +1,27 @@
+﻿using Assets._Scripts.Coordinator.MobActs;
+using UnityEngine;
+using Utils;
+
+namespace Coordinator.MobActs
+{
+    partial class ProjectileLaunchAct : GravityProjectileLaunchAct
+    {
+        private Transform _launchPos;
+
+        private void Awake()
+        {
+            _launchPos = transform.Find("@ProjectileLaunchAnchor");
+        }
+
+        public override void Launch()
+        {
+            if (_shootCnt <= 0)
+            {
+                return;
+            }
+            _shootCnt--;
+            ProjectileLaunchHelper.LaunchConstantDir(_target, _projectileIdx, _launchPos.position, transform.right);
+            _animator.SetInteger("ShootCnt", _shootCnt);
+        }
+    }
+}
