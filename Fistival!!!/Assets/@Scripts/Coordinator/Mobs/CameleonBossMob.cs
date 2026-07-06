828fe0e9b837aca9b76928a6571925dc5c8e3767
nuke1115 <nuclearbomb280@gmail.com>
2026-07-07 02:07:43 +0900

    feature: 보스몹의 머리가 플레이어를 따라다니도록 수정

--- Fistival!!!/Assets/@Scripts/Coordinator/Mobs/CameleonBossMob.cs
+++ Fistival!!!/Assets/@Scripts/Coordinator/Mobs/CameleonBossMob.cs
@@ -19,10 +19,20 @@ namespace Coordinator.Mobs
         protected bool _isActing;
         protected float _skillTime;
         protected IReadOnlyList<Transform> _points;
+        protected Transform _head;
+
+        [SerializeField]
+        protected float _tongueDuration;
+        [SerializeField]
+        protected float _tongueStayTime;
+        [SerializeField]
+        protected float _tongueLength;
 
         protected override void OnAwake()
         {
             base.OnAwake();
+            _head = transform.Find("@Head");
+            _acts[0] = GetComponent<TongueAct>();
         }
 
         public override void Init(CommonMobData data)
@@ -40,6 +50,7 @@ namespace Coordinator.Mobs
             var comps = GetComponentsInChildren<TouchDamageSkill>(true);
             comps[0].Init(data.PlayerHitboxLayer, _damage, _stunTime, _knockbackForce);
             comps[1].Init(data.PlayerHitboxLayer, _damage, _stunTime, _knockbackForce);
+            ((TongueAct)_acts[0]).Init(() => { _isActing = false; }, _animator, _tongueDuration, _tongueStayTime, _tongueLength, _groundLayer);
         }
 
         public void Init(CommonMobData data, IReadOnlyList<Transform> points)
@@ -50,17 +61,23 @@ namespace Coordinator.Mobs
 
         private void Update()
         {
+            
             if (_isActing)
             {
                 return;
             }
-
+            float dirSign = Mathf.Sign(transform.right.x);
+            Vector3 direction = -(_player.position - _head.position) * dirSign;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg * dirSign;
+            Vector3 localeular = _head.localEulerAngles;
+            _head.localRotation = Quaternion.Euler(localeular.x, localeular.y, angle);
+            return;
             if (_skillTime < _skillDelay)
             {
                 _skillTime += Time.deltaTime;
                 return;
             }
-
+            
             if (_stunCounter.IsCooldownEnded() == false)
             {
                 return;
