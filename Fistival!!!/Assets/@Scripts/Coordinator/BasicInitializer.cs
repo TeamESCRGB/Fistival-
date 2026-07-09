a97beed5536fc30d2951a44273e6d3435e39448d
nuke1115 <nuclearbomb280@gmail.com>
2026-07-09 22:55:16 +0900

    feature: 기본적인 Init함수 인터페이스 추가

---
+++ Fistival!!!/Assets/@Scripts/Coordinator/BasicInitializer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assets._Scripts.Coordinator
+{
+    public interface BasicInitializer
+    {
+        public void Init();
+    }
+}
