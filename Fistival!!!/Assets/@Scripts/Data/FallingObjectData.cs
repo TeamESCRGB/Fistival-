8b8fb917c4d63b25679f7d7edec77e184b54c289
nuke1115 <nuclearbomb280@gmail.com>
2026-07-12 02:42:42 +0900

    feature: FallingObject데이터 추가

---
+++ Fistival!!!/Assets/@Scripts/Data/FallingObjectData.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace Data
+{
+    public class FallingObjectData
+    {
+        public int IDX { get; set; }
+        public string PrefabKey {  get; set; }
+        public int BreakableLayerMask { get; set; }
+        public int[] SpawnableObjects { get; set; }
+        public int Damage {  get; set; }
+        public float StunTime { get; set; }
+        public Vector2 Scale { get; set; }
+        public float KnockBackForce { get; set; }
+        public string AnimControllerName { get; set; }
+    }
+}
