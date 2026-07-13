b778dd052ca70a5429747c059293fbbdeba15e09
nuke1115 <nuclearbomb280@gmail.com>
2026-07-13 20:30:58 +0900

    feature: Mob과 Object의 기본적인 스폰 핼퍼 추가

---
+++ Fistival!!!/Assets/@Scripts/Utils/ObjectSpawnHelper.cs
@@ -0,0 +1,35 @@
+﻿using Coordinator.Objects;
+using Manager;
+using UnityEngine;
+
+namespace Utils
+{
+    public static class ObjectSpawnHelper
+    {
+        public static ObjectCoordinator Spawn(int idx, Transform point)
+        {
+            if (Managers.Instance.DataManager.ObjectDataDict.TryGetValue(idx, out var data) == false)
+            {
+                return null;
+            }
+
+            var go = Managers.Instance.ResourceManager.Instantiate(data.PrefabKey);
+            if (go == null)
+            {
+                return null;
+            }
+
+            if (go.TryGetComponent<ObjectCoordinator>(out var comp))
+            {
+                go.transform.SetPositionAndRotation(point.transform.position, point.transform.rotation);
+                comp.Init(data);
+                return comp;
+            }
+            else
+            {
+                Managers.Instance.ResourceManager.Destroy(go);
+                return null;
+            }
+        }
+    }
+}
