73b48e97dc920f4fc5a1b73d85ee55064a36d00c
nuke1115 <nuclearbomb280@gmail.com>
2026-07-13 16:27:12 +0900

    feature: 플랫포머 보스 2페이즈 틀 추가

---
+++ Fistival!!!/Assets/@Scripts/Coordinator/Mobs/PlatformerBossPhase2Mob.cs
@@ -0,0 +1,93 @@
+using Coordinator.Movements;
+using Coordinator.Skills;
+using Data;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Coordinator.Mobs
+{
+    public class PlatformerBossPhase2Mob : MobCoordinatorBase, IPushable
+    {
+        private MobActBase[] _acts = new MobActBase[3];
+        private IReadOnlyList<Transform> _spawnPoints;
+        private Transform _player;
+        private bool _isActing;
+        private float _skillTime;
+
+
+        protected override void OnAwake()
+        {
+            base.OnAwake();
+        }
+
+        public override void Init(CommonMobData data)
+        {
+            base.Init(data);
+            _isActing = false;
+            _skillTime = _skillDelay;
+            _player = FindAnyObjectByType<PlayerCoordinator>().transform;
+            var touchDamages = GetComponentsInChildren<TouchDamageSkill>();
+            for (int i = 0; i < touchDamages.Length; i++)
+            {
+                touchDamages[i].Init(data.PlayerHitboxLayer, _damage, _stunTime, _knockbackForce);
+            }
+
+        }
+
+        public void Init(CommonMobData data, IReadOnlyList<Transform> spawnPoints)
+        {
+            _spawnPoints = spawnPoints;
+            Init(data);
+        }
+
+        private void Update()
+        {
+            if (_isActing)
+            {
+                return;
+            }
+
+
+            if (_skillTime < _skillDelay)
+            {
+                _skillTime += Time.deltaTime;
+                return;
+            }
+
+            _skillTime = 0;
+            _isActing = true;
+
+            _acts[1].Act();//UnityEngine.Random.Range(0, _acts.Length)
+        }
+
+        #region UnUsed
+        protected override void OnHPChanged(int old, int now, int delta)
+        {
+
+        }
+
+        public override void StunFor(float time)
+        {
+        }
+
+        public override void ReleaseStun()
+        {
+        }
+
+        public void PushTo(Vector2 force)
+        {
+
+        }
+
+        public override void OnStunEnd()
+        {
+
+        }
+
+        protected override void OnAggroStateChanged(bool isAggroOn, Collider2D player)
+        {
+
+        }
+        #endregion
+    }
+}
