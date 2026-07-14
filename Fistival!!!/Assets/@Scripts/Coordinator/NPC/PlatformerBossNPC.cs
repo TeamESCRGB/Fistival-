b78fff8374018c3a35912d8a87eeded66db9eb1a
nuke1115 <nuclearbomb280@gmail.com>
2026-07-14 17:59:32 +0900

    feature/플랫포머_보스_퇴장_후_NPC_틀_추가

---
+++ Fistival!!!/Assets/@Scripts/Coordinator/NPC/PlatformerBossNPC.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace Coordinator.NPC
+{
+    public class PlatformerBossNPC : MonoBehaviour
+    {
+        public void StartPlatformerStageClearedAnimation()
+        {
+
+        }
+    }
+}
