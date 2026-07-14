ef18a14f0df481ba07200eaa7b6e99f3d499e9ea
nuke1115 <nuclearbomb280@gmail.com>
2026-07-14 21:51:44 +0900

    remove: 컷씬 시스템 수정...

--- Fistival!!!/Assets/@Scripts/Coordinator/NPC/PlatformerBossNPC.cs
+++ Fistival!!!/Assets/@Scripts/Coordinator/NPC/PlatformerBossNPC.cs
@@ -1,15 +1,14 @@
-﻿using System;
+﻿using Manager;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using UI.Popup;
 using UnityEngine;
 
 namespace Coordinator.NPC
 {
     public class PlatformerBossNPC : MonoBehaviour
     {
-        public void StartPlatformerStageClearedAnimation()
-        {
 
-        }
     }
 }
