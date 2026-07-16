aebd013e88be64c0e9b0d961987ef5341d98b724
nuke1115 <nuclearbomb280@gmail.com>
2026-07-16 21:47:32 +0900

    add: 이름 추가

--- Fistival!!!/Assets/@Scripts/Data/TalkScriptData.cs
+++ Fistival!!!/Assets/@Scripts/Data/TalkScriptData.cs
@@ -5,6 +5,6 @@ namespace Data
     public class TalkScriptData
     {
         public string DataKey { get; set; }
-        public List<(string characterImg, string typeSFX, string script)> TalkData { get; set; }
+        public List<(string name, string characterImg, string typeSFX, string script)> TalkData { get; set; }
     }
 }
