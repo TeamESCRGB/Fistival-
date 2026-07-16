66c140efd4a6c09755c340e98b5ae71416e221dd
nuke1115 <nuclearbomb280@gmail.com>
2026-07-16 23:00:22 +0900

    fix: TalkData구조체로 분리

---
+++ Fistival!!!/Assets/@Scripts/Data/NonLodable/TalkData.cs
@@ -0,0 +1,10 @@
+﻿namespace Data.NonLodable
+{
+    public struct TalkData
+    {
+        public string name { get; set; }
+        public string characterImg { get; set; }
+        public string typeSFX { get; set; }
+        public string script { get; set; }
+    }
+}
