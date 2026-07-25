71b8374ecff4ecd4e45c5268e0b5ee48b5fd05b7
nuke1115 <nuclearbomb280@gmail.com>
2026-07-25 17:57:01 +0900

    feature: TouchDamage의 StandAlone버전 추가

---
+++ Fistival!!!/Assets/@Scripts/Coordinator/Skills/StandaloneTouchDamage.cs
@@ -0,0 +1,56 @@
+using Coordinator.Victims;
+using Manager;
+using UnityEngine;
+
+namespace Coordinator.Skills
+{
+    [RequireComponent(typeof(TouchDamageSkill))]
+    public class StandaloneTouchDamage : TouchDamageSkill
+    {
+        [SerializeField]
+        protected Vector2 _knockbackDir;
+        private Vector2 _knockbackDirNorm;
+        [SerializeField]
+        private LayerMask _attackables_ser;
+        [SerializeField]
+        private int _damage_ser;
+        [SerializeField]
+        private float _stunTime_ser;
+        [SerializeField]
+        private float _knockBackForce_ser;
+
+        private void Start()
+        {
+            _knockbackDirNorm = _knockbackDir.normalized;
+            Init(_attackables_ser, _damage_ser, _stunTime_ser, _knockBackForce_ser, true,false);
+        }
+
+        protected override void Attack(GameObject go)
+        {
+            if (go == null)
+            {
+                return;
+            }
+
+            if (((1 << go.layer) & _attackableLayers) == 0 || go.TryGetComponent<IAttackable>(out var comp) == false)
+            {
+                return;
+            }
+
+            var knockBackForce = _baseDamage * _knockbackForce;
+            var force = _knockbackDirNorm * knockBackForce;
+            Managers.Instance.AttackManager.RequestAttack(comp, this, _baseDamage, force, _baseStunTime);
+        }
+
+#if UNITY_EDITOR
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.red;
+
+            Vector3 knockbackVector = new Vector3(_knockbackDirNorm.x, _knockbackDirNorm.y, 0f) * _knockBackForce_ser;
+
+            Gizmos.DrawRay(transform.position, knockbackVector);
+        }
+#endif
+    }
+}
