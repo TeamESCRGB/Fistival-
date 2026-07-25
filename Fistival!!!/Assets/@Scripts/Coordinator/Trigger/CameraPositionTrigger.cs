8e1b7117cd04662a4db8c8b505cdd00702d50e26
nuke1115 <nuclearbomb280@gmail.com>
2026-07-25 20:00:10 +0900

    feature: 카메라 추적, 위치 지정 트리거 추가

---
+++ Fistival!!!/Assets/@Scripts/Coordinator/Trigger/CameraPositionTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace Coordinator.Trigger
+{
+    public class CameraPositionTrigger : MonoBehaviour
+    {
+        private Transform _cam;
+        [SerializeField]
+        private Transform _tarPosTransform;
+        private void Start()
+        {
+            _cam = Camera.main.transform;
+            var trigger = GetComponent<TouchTrigger>();
+            trigger.OnTriggerActivated -= SetCameraPos;
+            trigger.OnTriggerActivated += SetCameraPos;
+        }
+
+        private void SetCameraPos(GameObject _)
+        {
+            var pos = _cam.position;
+            var tarPos = _tarPosTransform.position;
+            pos.x = tarPos.x;
+            pos.y = tarPos.y;
+            _cam.position = pos;
+        }
+    }
+}
