b7cc658e6d241ed4fee9368791b401e424149f1a
nuke1115 <nuclearbomb280@gmail.com>
2026-07-26 14:35:50 +0900

    feature: BGM트리거들 추가

---
+++ Fistival!!!/Assets/@Scripts/Coordinator/Trigger/MobBGMTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Coordinator.Trigger
+{
+    public class MobBGMTrigger : BGMTriggerBase
+    {
+        private void Start()
+        {
+            GetComponentInChildren<HPCoordinator>().SubscribeOnDead(Play);
+        }
+    }
+}
---
+++ Fistival!!!/Assets/@Scripts/Coordinator/Trigger/TouchBGMTrigger.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Coordinator.Trigger
+{
+    public class TouchBGMTrigger : BGMTriggerBase
+    {
+        private void Start()
+        {
+            var trigger = GetComponent<TouchTrigger>();
+            trigger.OnTriggerActivated -= Play;
+            trigger.OnTriggerActivated += Play;
+        }
+
+        private void Play(GameObject triggered)
+        {
+            Play();
+        }
+    }
+}
