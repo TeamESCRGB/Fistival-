9d546fff36ce2fcfb0d14d82d5196920903e0c72
nuke1115 <nuclearbomb280@gmail.com>
2026-07-26 13:07:02 +0900

    fix: CHasePlatformerPatrolMob에서 Chasing변수 안올리던 문제 수정

--- Fistival!!!/Assets/@Scripts/Coordinator/Mobs/ChasePlatformerPatrolMob.cs
+++ Fistival!!!/Assets/@Scripts/Coordinator/Mobs/ChasePlatformerPatrolMob.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Data;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using UnityEngine;
@@ -10,15 +11,22 @@ namespace Coordinator.Mobs
         [SerializeField]
         private float _aggroSpeed;
 
+        public override void Init(CommonMobData data)
+        {
+            base.Init(data);
+            _animator.SetBool("Chasing", false);
+        }
 
         protected override void OnAggroStateChanged(bool isAggro, Collider2D collider)
         {
             if(isAggro)
             {
+                _animator.SetBool("Chasing", true);
                 _move.FollowTarget(_aggroSpeed,collider.gameObject.transform);
             }
             else
             {
+                _animator.SetBool("Chasing", false);
                 _move.StopFollow();
             }
         }
