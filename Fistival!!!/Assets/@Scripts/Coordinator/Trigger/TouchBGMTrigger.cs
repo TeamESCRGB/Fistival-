4be3cc888fbe9c1c582d1380a783d2a6ec9a9e76
nuke1115 <nuclearbomb280@gmail.com>
2026-07-26 15:26:13 +0900

    feature: TouchBGMTrigger에 playOnce옵션 추가

--- Fistival!!!/Assets/@Scripts/Coordinator/Trigger/TouchBGMTrigger.cs
+++ Fistival!!!/Assets/@Scripts/Coordinator/Trigger/TouchBGMTrigger.cs
@@ -1,18 +1,33 @@
-﻿using UnityEngine;
+﻿using Assets._Scripts.Coordinator;
+using UnityEngine;
 
 namespace Coordinator.Trigger
 {
-    public class TouchBGMTrigger : BGMTriggerBase
+    public class TouchBGMTrigger : BGMTriggerBase, IBasicInitializer
     {
+        [SerializeField]
+        private bool _playOncePerLife;
+        private bool _isPlayed;
         private void Start()
         {
             var trigger = GetComponent<TouchTrigger>();
             trigger.OnTriggerActivated -= Play;
             trigger.OnTriggerActivated += Play;
+            _isPlayed = false;
+        }
+
+        public void Init()
+        {
+            _isPlayed = false;
         }
 
         private void Play(GameObject triggered)
         {
+            if(_isPlayed && _playOncePerLife)
+            {
+                return;
+            }
+            _isPlayed = true;
             Play();
         }
     }
