538d8f8e346b6964d9ccebb7cf4622f888d49d59
nuke1115 <nuclearbomb280@gmail.com>
2026-07-26 15:08:15 +0900

    rename: MobBGMTrigger->MobDeathBGMTrigger

--- Fistival!!!/Assets/@Scripts/Coordinator/Trigger/MobDeathBGMTrigger.cs
+++ Fistival!!!/Assets/@Scripts/Coordinator/Trigger/MobDeathBGMTrigger.cs
@@ -4,7 +4,11 @@ using System.Text;
 
 namespace Coordinator.Trigger
 {
-    public class MobBGMTrigger : BGMTriggerBase
+    /// <summary>
+    /// 몹이 죽으면 그때부터 나올 bgm트리거
+    /// 
+    /// </summary>
+    public class MobDeathBGMTrigger : BGMTriggerBase
     {
         private void Start()
         {
