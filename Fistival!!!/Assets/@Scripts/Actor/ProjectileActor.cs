ba68d4fe685016420526379244ded395c43ef611
nuke1115 <nuclearbomb280@gmail.com>
2026-07-26 16:22:31 +0900

    fix: 발사체가 정확하게 방향을 보지 않는 문제 수정

--- Fistival!!!/Assets/@Scripts/Actor/ProjectileActor.cs
+++ Fistival!!!/Assets/@Scripts/Actor/ProjectileActor.cs
@@ -5,15 +5,21 @@ namespace Actor
     public class ProjectileActor
     {
         private Rigidbody2D _rb2d;
+        private Transform _transform;
 
         public ProjectileActor(Rigidbody2D rb2d)
         {
             _rb2d = rb2d;
+            _transform = rb2d.transform;
         }
 
         public void LookDir(Vector2 dir)
         {
-            _rb2d.MoveRotation(Mathf.Atan2(dir.y,dir.x) * Mathf.Rad2Deg);
+            var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+            _rb2d.MoveRotation(angle);
+            var rot = _transform.eulerAngles;
+            rot.z = angle;
+            _transform.eulerAngles = rot;
         }
     }
 }
