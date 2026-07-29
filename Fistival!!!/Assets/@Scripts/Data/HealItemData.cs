b49ea537667f4d208b89d588a407f44f1e3bcf25
nuke1115 <nuclearbomb280@gmail.com>
2026-07-30 02:18:31 +0900

    add: HealItemData에 PrefabKey추가

--- Fistival!!!/Assets/@Scripts/Data/HealItemData.cs
+++ Fistival!!!/Assets/@Scripts/Data/HealItemData.cs
@@ -3,6 +3,7 @@ namespace Data
     public class HealItemData
     {
         public int Idx { get; set; }
+        public string PrefabKey { get; set; }
         public int HealRate { get; set; }
         public string Image { get; set; }
         public string HealSFX { get; set; }
